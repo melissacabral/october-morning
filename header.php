4fae11ab14fd2b1b0d003c8b1422f29db1583cde
Melissa Cabral <melissacabral@gmail.com>
2017-10-25 08:05:32 -0700

    improving CSS

--- header.php
+++ header.php
@@ -6,13 +6,14 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="stylesheet" type="text/css" 
 			href="<?php echo get_stylesheet_uri(); ?>">
+			<link href="https://fonts.googleapis.com/css?family=Montserrat%7CSource+Sans+Pro" rel="stylesheet">
 
 	<?php wp_head(); //HOOK. required for the admin bar and plugins to work ?>
 </head>
 <body <?php body_class(); ?>>
 
 	<header class="header">
-		<div class="header-bar">
+		<div class="wrapper">
 			<h1 class="site-title"><a href="<?php echo home_url(); ?>">
 				<?php bloginfo( 'name' ); ?></a></h1>
 			<h2><?php bloginfo( 'description' ); ?></h2>
@@ -24,7 +25,12 @@
 				</ul>
 			</nav>
 
-			<?php get_search_form(); //searchform.php or default search form ?>
+			<?php 
+			//show the search if not on the front page
+			if( ! is_front_page() ):
+				get_search_form(); //searchform.php or default search form 
+			endif;
+			?>
 		</div>
 	</header>
 	<div class="wrapper">
